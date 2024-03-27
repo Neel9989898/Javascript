b4a81c0f59199835e506dff3e3fc46f4b289ef75
Neel Patel <neel.patel@rapidops.co>
2024-03-28 05:09:15 +0530

    huh

--- JS-Exercise-1/tst.js
+++ JS-Exercise-1/tst.js
@@ -1,12 +1,24 @@
 let num = 5;
 let result = '';
 
-for (let i = 0; i < num; i++) {
-    if (i === 0 || i === num - 1) {
-        result += ' * '.repeat(num) + '\n';
-    } else {
-        result += ' * '.repeat(1) + ' '.repeat((2*(num-2))+(num-1)) + '* ' + '\n';
+// for (let i = 0; i < num; i++) {
+//     if (i === 0 || i === num - 1) {
+//         result += ' * '.repeat(num) + '\n';
+//     } else {
+//         result += ' * '.repeat(1) + ' '.repeat((2*(num-2))+(num-1)) + '* ' + '\n';
+//     }
+// }
+
+// console.log(result);
+
+for(let i=0 ; i<num ;i++) {
+    for(let j=0; j<i;j++) {
+        if(num%2==0){
+            if(i==j){
+                result+=X
+            }
+        }
     }
 }
 
-console.log(result);
+console.log(result)
